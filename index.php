9e0bd0c7c35d698e16b95b66f2aaa4081442e6b3
SerigneFallouNiang <fallouniang776@gmail.com>
2024-02-27 22:15:09 +0000

    modification des noms

--- index.php
+++ index.php
@@ -4,66 +4,54 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>INSCRIPTION</title>
+    <title>Gestion Employés</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <?php
-        // Vérifier que le bouton ajouter a bien été cliqué
-        if(isset($_POST['boutton'])) {
-            // Extraction des informations envoyées dans des variables par la méthode POST
-            extract($_POST);
-            // Vérifier que tous les champs ont été remplis
-            if(!empty($nom) && !empty($prenom) && !empty($email) && !empty($password)) {
-                // Connexion à la base de données
+    <div class="container">
+        <a href="ajouter.php" class="Btn_add"> <img src="images/plus.png"> Ajouter</a>
+        
+        <table>
+            <tr id="items">
+                <th>Titre</th>
+                <th>Description</th>
+                <th>Statut</th>
+                <th>Modifier</th>
+                <th>Supprimer</th>
+            </tr>
+            <?php 
+                //inclure la page de connexion
                 include_once "connexion.php";
-                // Vérifier la connexion à la base de données
-                if($con) {
-                    // Requête d'ajout avec une requête préparée
-                    $stmt = $con->prepare("INSERT INTO utilisateur (nom, prenom, email, password) VALUES (?, ?, ?, ?)");
-                    // Sécurisation du mot de passe en le hachant (ici en utilisant la fonction password_hash)
-                    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
-                    // Liaison des paramètres
-                    $stmt->bind_param("ssss", $nom, $prenom, $email, $hashed_password);
-                    // Exécution de la requête
-                    if($stmt->execute()) {
-                        // Redirection si l'inscription a réussi
-                        header("location: index.php");
-                        exit(); // Terminer le script après la redirection
-                    } else {
-                        $message = "Erreur lors de l'ajout de l'utilisateur.";
+                //requête pour afficher la liste des idee
+                $req = mysqli_query($con , "SELECT * FROM idee");
+                if(mysqli_num_rows($req) == 0){
+                    //s'il n'existe pas d'idee dans la base de donné , alors on affiche ce message :
+                    echo "Il n'y a pas encore d'employé ajouter !" ;
+                    
+                }else {
+                    //si non , affichons la liste de tous les idee
+                    while($row=mysqli_fetch_assoc($req)){
+                        ?>
+                        <tr>
+                            <td><?=$row['titre']?></td>
+                            <td><?=$row['description']?></td>
+                            <td><?=$row['statut']?></td>
+                            <!--Nous alons mettre l'id de chaque employé dans ce lien -->
+                            <td><a href="modifier.php?id=<?=$row['id']?>"><img src="images/pen.png"></a></td>
+                            <td><a href="supprimer.php?id=<?=$row['id']?>"><img src="images/trash.png"></a></td>
+                        </tr>
+                        <?php
                     }
-                } else {
-                    $message = "Erreur de connexion à la base de données.";
+                    
                 }
-            } else {
-                $message = "Veuillez remplir tous les champs !";
-            }
-        }
-    ?>
-    <div class="form">
-        <a href="index.php" class="back_btn"><img src="images/back.png"> Retour</a>
-        <h2>S'INSCRIRE</h2>
-        <p class="erreur_message">
-            <?php
-            // Si la variable message existe, affichons son contenu
-            if(isset($message)) {
-                echo $message;
-            }
             ?>
-        </p>
-        <form action="" method="POST">
-            <label>Nom</label>
-            <input type="text" name="nom">
-            <label>Prénom</label>
-            <input type="text" name="prenom">
-            <label>Email</label>
-            <input type="email" name="email">
-            <label>Mot de Passe</label>
-            <input type="password" name="password">
-            
-            <input type="submit" value="INSCRIRE" name="boutton">
-        </form>
+      
+         
+        </table>
+   
+   
+   
+   
     </div>
 </body>
 </html>
