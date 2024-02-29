3febaef5f44867de035a7493aaa8ab2650d310da
SerigneFallouNiang <fallouniang776@gmail.com>
2024-02-29 22:19:26 +0000

    ajout de la page connection

--- connecter.php
+++ connecter.php
@@ -15,21 +15,19 @@
        // Vérifier que le bouton ajouter a bien été cliqué
        if(isset($_POST['button'])){
            // Vérifier que tous les champs ont été remplis
-           if(!empty($_POST['prenom']) && !empty($_POST['nom']) && !empty($_POST['email']) && !empty($_POST['password'])){
+           if(!empty($_POST['email']) && !empty($_POST['password'])){
+            $email = $_POST['email'];
+            $password = $_POST['password'];
                 // Connexion à la base de données
                 include_once "connexion.php";
-                // Requête d'ajout avec requête préparée
-                $stmt = $con->prepare("INSERT INTO utilisateur (prenom, nom, email, password) VALUES (?, ?, ?, ?)");
-                // Liaison des paramètres
-                $stmt->bind_param("ssss", $_POST['prenom'], $_POST['nom'], $_POST['email'], $_POST['password']);
-                // Exécution de la requête
-                if($stmt->execute()){
-                    // Redirection si l'ajout a réussi
+                $select = mysqli_query($con,"SELECT * FROM utilisateur where email ='$email' AND password = '$password'");
+                $row = mysqli_fetch_array($select);
+                if($row){//si la requête a été effectuée avec succès , on fait une redirection
                     header("location: index.php");
-                    exit(); // Terminer le script après la redirection
-                } else {
-                    $message = "Erreur lors de l'inscription. Veuillez réessayer.";
+                }else {//si non
+                    $message = "Employé non trouvé";
                 }
+              
            } else {
                $message = "Veuillez remplir tous les champs !";
            }
@@ -38,7 +36,7 @@
     ?>
     <div class="form">
         <a href="index.php" class="back_btn"><img src="images/back.png"> Retour</a>
-        <h2>S'inscrire :</h2>
+        <h2>connexion :</h2>
         <p class="erreur_message">
             <?php 
             // si la variable message existe , affichons son contenu
@@ -48,10 +46,6 @@
             ?>
         </p>
         <form action="" method="POST">
-            <label>Prénom</label>
-            <input type="text" name="prenom">
-            <label>Nom</label>
-            <input type="text" name="nom">
             <label>Email</label>
             <input type="text" name="email">
             <label>Mot de passe</label>
@@ -61,3 +55,9 @@
     </div>
 </body>
 </html>
+
+
+
+
+
+
